a4cb1da21eb5f300b232361e208b13e773171265
Никулин Павел <XpeHo3eM@gmail.com>
2023-05-29 16:53:47 +0400

    Переделаны mpa и genres из enum в классы

--- src/main/java/ru/yandex/practicum/filmorate/model/Genre.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Genre.java
@@ -4,14 +4,33 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
-public enum Genre {
-    COMEDY(1, "Комедия"),
-    DRAMA(2, "Драма"),
-    CARTOON(3, "Мультфильм"),
-    THRILLER(4, "Триллер"),
-    DOCUMENTARY(5, "Документальный"),
-    ACTION_MOVIE(6, "Боевик");
+public class Genre {
+    public static final Genre COMEDY = new Genre(1, "Комедия");
+    public static final Genre DRAMA = new Genre(2, "Драма");
+    public static final Genre CARTOON = new Genre(3, "Мультфильм");
+    public static final Genre THRILLER = new Genre(4, "Триллер");
+    public static final Genre DOCUMENTARY = new Genre(5, "Документальный");
+    public static final Genre ACTION_MOVIE = new Genre(6, "Боевик");
+    private static final Map<Integer, Genre> idToGenres = new HashMap<>() {{
+        put(COMEDY.id, COMEDY);
+        put(DRAMA.id, DRAMA);
+        put(CARTOON.id, CARTOON);
+        put(THRILLER.id, THRILLER);
+        put(DOCUMENTARY.id, DOCUMENTARY);
+        put(ACTION_MOVIE.id, ACTION_MOVIE);
+    }};
+    private static final Map<String, Genre> genreNameToGenre = new HashMap<>() {{
+        put("COMEDY", COMEDY);
+        put("DRAMA", DRAMA);
+        put("CARTOON", CARTOON);
+        put("THRILLER", THRILLER);
+        put("DOCUMENTARY", DOCUMENTARY);
+        put("ACTION_MOVIE", ACTION_MOVIE);
+    }};
 
     private final int id;
     private final String name;
@@ -29,14 +48,12 @@ public enum Genre {
         return id;
     }
 
+    public static Genre valueOf(String genre) {
+        return genreNameToGenre.get(genre);
+    }
+
     @JsonCreator
     public static Genre forValues(@JsonProperty("id") int id) {
-        for (Genre genre : Genre.values()) {
-            if (genre.id == id) {
-                return genre;
-            }
-        }
-
-        return null;
+        return idToGenres.get(id);
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/Mpa.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Mpa.java
@@ -4,13 +4,30 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
-public enum Mpa {
-    G(1, "G"),
-    PG(2, "PG"),
-    PG13(3, "PG-13"),
-    R(4, "R"),
-    NC17(5, "NC-17");
+public class Mpa {
+    public static final Mpa G = new Mpa(1, "G");
+    public static final Mpa PG = new Mpa(2, "PG");
+    public static final Mpa PG13 = new Mpa(3, "PG-13");
+    public static final Mpa R = new Mpa(4, "R");
+    public static final Mpa NC17 = new Mpa(5, "NC-17");
+    private static final Map<Integer, Mpa> idToMpas = new HashMap<>() {{
+        put(G.id, G);
+        put(PG.id, PG);
+        put(PG13.id, PG13);
+        put(R.id, R);
+        put(NC17.id, NC17);
+    }};
+    private static final Map<String, Mpa> mpaNameToMpa = new HashMap<>() {{
+        put("G", G);
+        put("PG", PG);
+        put("PG13", PG13);
+        put("R", R);
+        put("NC17", NC17);
+    }};
 
     private final int id;
     private final String name;
@@ -28,14 +45,12 @@ public enum Mpa {
         return id;
     }
 
+    public static Mpa valueOf(String mpa) {
+        return mpaNameToMpa.get(mpa);
+    }
+
     @JsonCreator
     public static Mpa forValues(@JsonProperty("id") int id) {
-        for (Mpa mpa : Mpa.values()) {
-            if (mpa.id == id) {
-                return mpa;
-            }
-        }
-
-        return null;
+        return idToMpas.get(id);
     }
 }
